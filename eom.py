9369fc0c8d9424120a19ed9c7e9ba811ad4e6a55
smokethis <64678625+smokethis@users.noreply.github.com>
2026-07-08 18:27:48 +0100

    Update schema to firmware v2.0 compliance

--- eom.py
+++ eom.py
@@ -22,37 +22,27 @@ class EdgeOMaticConfig(Struct):
     bt_display_name: str
     bt_on: bool
     force_bt_coex: bool
-    led_brightness: int # originally `byte`
+    led_brightness: int
     websocket_port: int
     use_ssl: bool
     hostname: str
-    motor_start_speed: int # originally `byte`
-    motor_max_speed: int # originally `byte`
+    motor_min_speed: int
+    motor_max_speed: int
     motor_ramp_time_s: int
-    edge_delay: int
-    max_additional_delay: int
-    minimum_on_time: int
+    cooldown_delay_ms: int
+    cooldown_random_ms: int
+    arousal_holdoff_ms: int
     screen_dim_seconds: int
     screen_timeout_seconds: int
     reverse_menu_scroll: bool
-    pressure_smoothing: int # originally `byte`
+    pressure_smoothing: int
     classic_serial: bool
     sensitivity_threshold: int
     update_frequency_hz: int
-    sensor_sensitivity: int # originally `byte`
+    sensor_sensitivity: int
     use_average_values: bool
     vibration_mode: VibrationMode
-    use_post_orgasm: bool
-    clench_pressure_sensitivity: int
-    clench_time_to_orgasm_ms: int
-    clench_detector_is_edging: bool = msgspec.field(name="clench_detector_in_edging") # ?????
-    auto_edging_duration_minutes: int
-    post_orgasm_duration_seconds: int
-    edge_menu_lock: bool
-    post_orgasm_menu_lock: bool
-    max_clench_duration_ms: int
-    clench_time_threshold_ms: int
-    # added in version 2?
+    # ADDED FROM v2.0 firmware config file
     _filename: str
     store_command_history: bool
     console_basic_mode: bool
@@ -60,6 +50,25 @@ class EdgeOMaticConfig(Struct):
     language_file_name: str
     remote_update_url: str
     version: int = msgspec.field(name="$version")
+    mdns_enabled: bool
+    denial_count_mode: int
+    arousal_decay_rate: int
+    od_mode: int
+    od_sustained_threshold: int
+    od_sustained_fallback_ms: int
+    od_sustained_dropout_ms: int
+    od_peak_min_amplitude: int
+    od_rhythmic_min_peaks: int
+    od_rhythmic_interval_min_ms: int
+    od_rhythmic_interval_max_ms: int
+    od_rhythmic_interval_variance_ms: int
+    od_rhythmic_timeout_ms: int
+    od_arousal_gate_percent: int
+    od_recovery_ms: int
+    od_clench_arousal_boost: bool
+    od_clench_arousal_boost_amount: int
+    od_detection_armed: bool
+    
 
 class EdgeOMaticReadings(Struct):
     pressure: int
@@ -68,9 +77,18 @@ class EdgeOMaticReadings(Struct):
     arousal: int
     millis: int
     run_mode: ControlMode = msgspec.field(name="runMode")
-    permit_orgasm: bool = msgspec.field(name="permitOrgasm")
-    post_orgasm: bool = msgspec.field(name="postOrgasm")
-    lock: bool
+
+    # REMOVED IN firmware v2.0 broadcast.c
+    # permit_orgasm: bool = msgspec.field(name="permitOrgasm")
+    # post_orgasm: bool = msgspec.field(name="postOrgasm")
+    # lock: bool
+
+    # NEW FIELDS FROM firmware v2.0 broadcast.c:
+    detect_state: str = msgspec.field(name="detectState")
+    detect_baseline: int = msgspec.field(name="detectBaseline")
+    detect_peak_count: int = msgspec.field(name="detectPeakCount")
+    detect_sustained_ms: int = msgspec.field(name="detectSustainedMs")
+    detect_last_interval_ms: int = msgspec.field(name="detectLastIntervalMs")
 
 class EdgeOMaticInfo(Struct):
     device: str
